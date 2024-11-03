450cd60ae6c4d86798985f53f93a0b6b58c4f1a5
mohamedhussieen <69179345+mohamedhussieen@users.noreply.github.com>
2024-11-03 10:55:00 +0200

    [191] [Time Beats: 100.00%] [Memory Beats: 17.19%] - LeetPush

---
+++ 191-Number-of-1-Bits.php
@@ -0,0 +1,11 @@
+class Solution {
+    /**
+     * @param Integer $n * @return Integer */ function hammingWeight($n) {
+        $count = 0; while ($n != 0) {
+            if ($n & 1 == 1) { $count++;
+            }
+            $n = $n >> 1;
+        }
+        return $count;
+    }
+}
