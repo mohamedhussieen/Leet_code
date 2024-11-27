42b2bc7dad0e611a352b9e667a583da6cf0d3d0c
mohamedhussieen <69179345+mohamedhussieen@users.noreply.github.com>
2024-11-27 11:11:51 +0200

    [100] [Time Beats: 100.00%] [Memory Beats: 5.47%] - LeetPush

---
+++ 100-Same-Tree.php
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($val = 0, $left = null, $right = null) {
+ *         $this->val = $val;
+ *         $this->left = $left;
+ *         $this->right = $right;
+ *     }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param TreeNode $p
+     * @param TreeNode $q
+     * @return Boolean
+     */
+    function isSameTree($p, $q) {
+        if ($p->val !== $q->val) return false;
+
+        if ($p == null && $q == null) return true;
+
+        return $this->isSameTree($p->left, $q->left) && $this->isSameTree($p->right, $q->right);
+    }
+}
